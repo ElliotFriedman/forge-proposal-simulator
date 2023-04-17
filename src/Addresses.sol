51805815851dedddb9229667d5e054ee41e58d28
Elliot <elliotfriedman3@gmail.com>
2023-04-17 16:10:35 -0700

    natspec and interface import

--- src/Addresses.sol
+++ src/Addresses.sol
@@ -2,8 +2,9 @@
 pragma solidity =0.8.13;
 
 import {Test} from "@forge-std/Test.sol";
+import {IAddresses} from "./IAddresses.sol";
 
-contract Addresses is Test {
+contract Addresses is IAddresses, Test {
     mapping(string => address) private _mainnet;
 
     struct RecordedAddress {
@@ -22,25 +23,37 @@ contract Addresses is Test {
         /// _addMainnet("V1_CORE", 0xEC7AD284f7Ad256b64c6E69b84Eb0F48f42e8196);
     }
 
-    function _addMainnet(string memory name, address addr) private {
+    /// @notice only call this function during setup, not during deploy stage of a proposal
+    /// @param name of the contract
+    /// @param addr address of the contract
+    function _addMainnet(string memory name, address addr) internal {
         _mainnet[name] = addr;
         vm.label(addr, name);
     }
-
+    
+    /// getter function to return the contract address given a contract name
+    /// @param name of the contract
     function mainnet(string memory name) public view returns (address) {
         return _mainnet[name];
     }
 
+    /// @notice call this function only during the deploy stage of a proposal
+    /// @param name of the contract
+    /// @param addr address of the contract
     function addMainnet(string memory name, address addr) public {
         _addMainnet(name, addr);
 
         recordedAddresses.push(RecordedAddress({name: name, addr: addr}));
     }
 
+    /// @notice clear all recorded addresses that were added using the addMainnet method
+    /// only deletes the array, data stored in the mapping is preserved
     function resetRecordingAddresses() external {
         delete recordedAddresses;
     }
 
+    /// @notice return all addresses and names of contracts that were deployed using the
+    /// addMainnet method
     function getRecordedAddresses()
         external
         view
