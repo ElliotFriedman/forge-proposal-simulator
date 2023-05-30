c060cd5b3453d315ec5250db4dc5716a59ca0e3f
Elliot <elliotfriedman3@gmail.com>
2023-05-30 11:34:31 -0700

    float pragmas

--- src/Addresses.sol
+++ src/Addresses.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
-pragma solidity =0.8.13;
+pragma solidity ^0.8.0;
 
 import {Test} from "@forge-std/Test.sol";
 import {IAddresses} from "./IAddresses.sol";
